68a3cbc5b93705051fba9b548f7259ee7ecf9100
Nurul Asif <nasif42069@gmail.com>
2025-12-19 17:51:19 +0600

    first

---
+++ store/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Product
+
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_dislay= ('product_name', 'price' , 'stock', 'category', 'is_available')
+    prepopulated_fields = {'slug': ('product_name',)}
+    
+
+admin.site.register(Product,ProductAdmin)
+
+
+
+
+
---
+++ store/models.py
@@ -0,0 +1,23 @@
+from django.urls import reverse
+from django.db import models
+from category.models import Category
+
+# Create your models here.
+class Product(models.Model):
+    product_name= models.CharField(max_length=200,unique=True)
+    description = models.TextField (max_length=300,unique=True)
+    slug= models.SlugField(max_length=50)
+    price= models.IntegerField()
+    images = models.ImageField(upload_to ='photos/products', blank=True)
+    stock = models.IntegerField()
+    is_available= models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    
+    
+    def get_url(self):
+        return reverse('product_detail', args=[self.category.slug, self.slug])
+    
+    
+    def __str__(self):
+        return self.product_name
